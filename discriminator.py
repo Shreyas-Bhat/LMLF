be88be17294f84c38588cfe83844d346efb72ccc
Shreyas Bhat <56354373+Shreyas-Bhat@users.noreply.github.com>
2023-03-20 09:02:45 +0530

    Updating files

---
+++ discriminator.py
@@ -0,0 +1,270 @@
+
+import torch
+import transformers
+import json
+from sklearn.model_selection import train_test_split
+from transformers import GPT2ForSequenceClassification, Trainer, TrainingArguments
+from transformers import GPT2TokenizerFast
+
+from torch.utils.data import DataLoader
+from transformers import AdamW
+
+
+def readsmiles(datafile):
+    fp=open(datafile,"r")
+    samples=[]
+    labels=[]
+    count=0
+    for line in fp:
+        if len(line)<5:
+            continue
+        # print(line)
+        term=line.split()
+    #     # print("term", term)
+        samples.append(term)
+    #     #print(len(line.split("$")))
+    #     term=line.split("$")[1]
+        
+    #     if term.strip()=="0":
+    #         label=0
+    #     else:
+    #         label=1
+        
+    #     sample=line.split("$")[0]
+    #     # sample=sample+"$"
+    #     samples.append(sample)
+    #     # print(sample)
+    #     count=count+1    
+    #     labels.append(label)
+    # return samples, labels
+
+        
+        # if term.strip()=="0":
+        #     label=0
+        # else:
+        #     label=1
+        
+        # sample=line.split("$")[0]
+        # # sample=sample+"$"
+        # samples.append(sample)
+        # # print(sample)
+        # count=count+1    
+        # labels.append(label)
+        # print(samples)
+    return samples
+
+class SmilesDataset(torch.utils.data.Dataset):
+    def __init__(self, encodings, labels):
+        self.encodings = encodings
+        self.labels = labels
+
+    def __getitem__(self, idx):
+        item = {key: torch.tensor(val[idx]) for key, val in self.encodings.items()}
+        item['labels'] = torch.tensor(self.labels[idx])
+        return item
+
+    def __len__(self):
+        return len(self.labels)
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+# import ast 
+# smiles_str = json.load(open('/home/f20190969/BioLLM/data/folds.json'))
+# smiles_dict=json.loads(smiles_str)-wont work with single quotes
+# smiles_dict=ast.literal_eval(smiles_str)
+#print("dict:", smiles_dict[0])
+
+# fp=open('/home/f20190969/BioLLM/data/output_class_single.txt', 'r')
+# alldata=fp.readlines()
+# #print(alldata)
+# fp.close()
+
+
+# from transformers.trainer import DataCollatorWithPadding
+
+# def generate_dataset(tokenizer, train_filename, test_filename):
+    #print(smiles_dict)
+    # train_indices=smiles_dict[fold]['Train']
+    # test_indices=smiles_dict[fold]['Test']
+    # train_data=[alldata[i]+'\n' for i in train_indices]
+    # test_data=[alldata[i]+'\n' for i in test_indices]
+    # fp=open('/home/datalab/BioLLM/data/smiles_total.txt', 'w')
+    # fp.writelines(train_data)
+    # fp.close()
+    # fp=open('/home/datalab/BioLLM/data/test_smiles.txt', 'w')
+    # fp.writelines(test_data)
+    # fp.close() 		
+    
+    # train_samples, train_labels = readsmiles('/home/datalab/BioLLM/data/smiles_total_4.txt')
+    # print(len(train_labels))
+    # test_samples, test_labels = readsmiles('/home/datalab/BioLLM/data/test_smiles4.txt')
+    # train_samples, val_samples, train_labels, val_labels  = train_test_split(train_samples,train_labels, test_size=0.1)
+    # train_encodings = tokenizer(train_samples, truncation=True, padding = True)
+    # val_encodings = tokenizer(val_samples, truncation = True, padding = True)
+    # test_encodings = tokenizer(test_samples, truncation = True, padding = True)
+    # train_dataset = SmilesDataset(train_encodings, train_labels)
+    # val_dataset = SmilesDataset(val_encodings, val_labels)
+    # test_dataset = SmilesDataset(test_encodings, test_labels)
+    # return train_dataset, val_dataset, test_dataset
+
+
+# from transformers import DataCollatorWithPadding
+
+
+
+# for fold in range(1): #smiles_dict.keys():
+
+   
+#     tokenizer = GPT2TokenizerFast.from_pretrained('gpt2', truncation=True)
+#     tokenizer.pad_token = tokenizer.eos_token
+#     tokenizer.pad_token_id = tokenizer.eos_token_id
+
+#     data_collator = DataCollatorWithPadding(tokenizer=tokenizer, return_tensors="pt")
+#     tokenizer.pad_token = tokenizer.eos_token
+#     train_dataset, val_dataset, test_dataset = generate_dataset(tokenizer, '/home/datalab/BioLLM/data/smiles_total_4.txt', '/home/datalab/BioLLM/data/test_smiles4.txt')
+#     model=GPT2ForSequenceClassification.from_pretrained("gpt2")
+#     model.config.pad_token_id = model.config.eos_token_id
+#     model.to(device)
+#     #print(train_dataset)
+#     training_args = TrainingArguments(
+#     output_dir='./results_disc_new_single_4',          # output directory
+#     overwrite_output_dir = True ,
+#     num_train_epochs=5,              # total number of training epochs
+#     per_device_train_batch_size=1,  # batch size per device during training
+#     per_device_eval_batch_size=1,   # batch size for evaluation
+#     warmup_steps=500,                # number of warmup steps for learning rate scheduler
+#     weight_decay=0.01,               # strength of weight decay
+#     logging_dir='./logs_disc_new_single_4',            # directory for storing logs
+#     logging_steps=10,
+#     save_total_limit=5
+#     )
+
+
+#     trainer = Trainer(
+#     model=model,                         # the instantiated model to be trained
+#     args=training_args,                  # training arguments, defined above
+#     train_dataset=train_dataset,         # training dataset
+#     eval_dataset=val_dataset ,  
+#     data_collator=data_collator          # evaluation dataset
+#     )
+
+    # trainer.train()
+    # torch.save(model,"gpt2_disc_4.pt")
+
+# #model.train()
+    
+
+# """train_loader = DataLoader(train_dataset, batch_size=16, shuffle=True)
+
+
+# optim = AdamW(model.parameters(), lr=5e-5)
+
+# for epoch in range(3):
+#     for batch in train_loader:
+#         optim.zero_grad()
+#         input_ids = batch['input_ids'].to(device)
+#         attention_mask = batch['attention_mask'].to(device)
+#         labels = batch['labels'].to(device)
+#         outputs = model(input_ids, attention_mask=attention_mask, labels=labels)
+#         loss = outputs[0]
+#         loss.backward()
+#         optim.step()
+
+# model.eval()"""
+from transformers import pipeline
+from tqdm.auto import tqdm
+fold = 0 
+test_samples = readsmiles('/home/datalab/BioLLM/results_smiles_gpt2_10000_1.txt')
+print(test_samples)
+#model=torch.load("fold"+str(fold)+"gpt2_single.pt")
+#device=torch.device("cpu")
+#from transformers.pipelines.pt_utils import KeyDataset
+# model.cuda()
+model=torch.load("/home/datalab/BioLLM/gpt2_disc_4.pt")
+from transformers import pipeline
+from tqdm.auto import tqdm
+device=torch.device("cuda")
+#from transformers.pipelines.pt_utils import KeyDataset
+#model.cuda()
+# tokenizer = GPT2TokenizerFast.from_pretrained('distilgpt2', truncation=True)
+# tokenizer.pad_token = tokenizer.eos_token
+
+# generator = pipeline(task="text-generation",  model=model, tokenizer=tokenizer)
+# tokenizer_kwargs = {'padding':True,'truncation':True,'max_length':512}
+# generator("0")
+tokenizer = GPT2TokenizerFast.from_pretrained('gpt2', truncation=True)
+tokenizer.pad_token = tokenizer.eos_token
+tokenizer.pad_token_id = tokenizer.eos_token_id
+
+# data_collator = DataCollatorWithPadding(tokenizer=tokenizer, return_tensors="pt")
+# tokenizer.pad_token = tokenizer.eos_token
+classifier = pipeline(task="text-classification", model=model.to('cpu'), tokenizer=tokenizer)
+tokenizer_kwargs = {'padding':True,'truncation':True,'max_length':512}
+predictions=[]
+import csv
+
+# Define the file path where the CSV file will be written
+csv_file = './proxy_predictions_2.csv'
+
+# Define the column names for the CSV file
+columns = ['smiles', 'label']
+with open(csv_file, mode='w') as file:
+    writer = csv.writer(file)
+    writer.writerow(columns)
+    for i in range(len(test_samples)):
+        predictions = classifier(str(test_samples[i]), **tokenizer_kwargs)[0]
+        # print(predictions)
+        row = [str(test_samples[i]), str(predictions['label'])]
+        writer.writerow(row)
+    # print(predictions[0])
+
+
+from evaluate import evaluator
+import datasets
+from datasets import Dataset
+# fold = 0
+# #torch.save(model,"fold"+str(fold)+"gpt2.pt")
+# task_evaluator=evaluator('text-classification')
+# model=torch.load("fold"+str(fold)+"gpt2_single.pt")
+#train_dataset, val_dataset, test_dataset = generate_dataset(smiles_dict, fold, alldata, tokenizer, '/home/f20190969/BioLLM/data/train_smiles_currentfold.txt', '/home/f20190969/BioLLM/data/test_smiles_currentfold.txt')
+#print(test_dataset, "here")
+    
+# train_samples, train_labels = readsmiles('/home/f20190969/BioLLM/data/train_smiles_currentfold.txt')
+# test_samples, test_labels = readsmiles('/home/f20190969/BioLLM/data/test_smiles_currentfold.txt')
+# train_samples, val_samples, train_labels, val_labels = train_test_split(train_samples, train_labels, test_size=0.1)
+# train_encodings = tokenizer(train_samples, truncation=True, padding = True)
+# val_encodings = tokenizer(val_samples, truncation = True, padding = True)
+# test_encodings = tokenizer(test_samples, truncation = True, padding = True)
+# import pyarrow as pa
+# import pandas as pd 
+# df = pd.read_csv('/home/f20190969/BioLLM/data/test_smiles_currentfold.csv')
+# # ds = Dataset(df)
+# def convert_to_table(smiles_dataset):
+#     # Extract the data from SmilesDataset object
+#     data = smiles_dataset.data
+#     # Convert the data into a pyarrow Table object
+#     table = pa.Table.from_arrays(data, names=smiles_dataset.columns)
+#     return table
+
+# smiles_dataset = SmilesDataset(test_encodings, test_labels)
+# table = convert_to_table(df)
+# ds = Dataset(table)
+# from datasets import load_dataset
+# dataset = load_dataset('csv', data_files = {'train': ['/home/datalab/BioLLM/data/smiles_total_withsplit.csv'], 'test': '/home/datalab/BioLLM/data/test_smiles_withsplit.csv'})
+# print(dataset['train'][0])
+# results = task_evaluator.compute(
+
+#     model_or_pipeline=model,
+#     tokenizer = GPT2TokenizerFast.from_pretrained('gpt2', truncation=True),
+
+#     data=dataset['test'],
+
+#     metric="f1",
+
+#     label_mapping={"LABEL_0": 0.0, "LABEL_1": 1.0},
+
+# )
+# print(results)
+
+
+
+
+
---
+++ generation2.py
@@ -0,0 +1,237 @@
+
+import torch
+torch.cuda.empty_cache()
+import transformers
+
+import json
+from sklearn.model_selection import train_test_split
+from transformers import GPT2ForSequenceClassification, Trainer, TrainingArguments
+from transformers import GPT2TokenizerFast
+
+from torch.utils.data import DataLoader
+from transformers import AdamW
+
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+def readsmiles(datafile):
+    fp=open(datafile,"r")
+    samples=[]
+    labels=[]
+    count=0
+    for line in fp:
+        if len(line)<5:
+            continue
+        # print(line)
+        # term=line.split("$")[1]
+        
+        # if term.strip()=="-1":
+        #     label=0
+        # else:
+        #     label=1
+        
+        sample=line.split("$")[0]
+        # sample=sample+"$"
+        sample=sample
+        samples.append(sample)
+        count = count + 1
+        
+        labels.append(count)
+    return samples, labels
+		
+train_samples, train_labels = readsmiles('/home/datalab/BioLLM/data/g2wrapped.txt')
+test_samples, test_labels = readsmiles('/home/datalab/BioLLM/data/allsmiles_2.txt')
+# print("samples", train_samples, "labels", train_labels)
+
+# from sklearn.model_selection import train_test_split
+train_samples, val_samples, train_labels, val_labels = train_test_split(train_samples, train_labels, test_size=0.1)
+
+from transformers import GPT2TokenizerFast
+from transformers import BioGptTokenizer, BioGptForCausalLM
+# from transformers import AutoTokenizer, T5ForConditionalGeneration
+tokenizer = GPT2TokenizerFast.from_pretrained('distilgpt2', truncation=True)
+# from transformers import AutoTokenizer, AutoModelForMaskedLM
+# # tokenizer = AutoTokenizer.from_pretrained("microsoft/biogpt")
+# tokenizer = BioGptTokenizer.from_pretrained("microsoft/biogpt")
+# # tokenizer = AutoTokenizer.from_pretrained("mrm8488/chEMBL26_smiles_v2")
+# tokenizer = AutoTokenizer.from_pretrained("ncfrey/ChemGPT-4.7M")
+
+
+# # tokenizer = AutoTokenizer.from_pretrained("laituan245/molt5-small", model_max_length=512)
+
+
+tokenizer.pad_token = tokenizer.eos_token
+if tokenizer.pad_token is None:
+    tokenizer.add_special_tokens({'pad_token': '[PAD]'})
+
+
+# train_encodings = tokenizer(train_samples)
+# val_encodings = tokenizer(val_samples)
+# test_encodings = tokenizer(test_samples)
+train_encodings = tokenizer(train_samples, truncation=True, padding = True)
+val_encodings = tokenizer(val_samples, truncation = True, padding = True)
+test_encodings = tokenizer(test_samples, truncation = True, padding = True)
+
+
+class SmilesDataset(torch.utils.data.Dataset):
+    def __init__(self, encodings, labels):
+        self.encodings = encodings
+        self.labels = labels
+
+    def __getitem__(self, idx):
+        item = {key: torch.tensor(val[idx]) for key, val in self.encodings.items()}
+        item['labels'] = torch.tensor(self.labels[idx])
+        return item
+
+    def __len__(self):
+        return len(self.labels)
+
+train_dataset = SmilesDataset(train_encodings, train_labels)
+val_dataset = SmilesDataset(val_encodings, val_labels)
+test_dataset = SmilesDataset(test_encodings, test_labels)
+
+
+# # print(train_dataset)
+# from transformers import  Trainer, TrainingArguments
+# #model = DistilBertForSequenceClassification.from_pretrained("distilbert-base-uncased")
+from transformers import AutoModelForCausalLM, TrainingArguments, Trainer
+from transformers import DataCollatorForLanguageModeling
+model=AutoModelForCausalLM.from_pretrained("distilgpt2")
+# # model = AutoModelForCausalLM.from_pretrained("mrm8488/chEMBL26_smiles_v2")
+# # model = AutoModelForCausalLM.from_pretrained("microsoft/biogpt")
+# model = BioGptForCausalLM.from_pretrained("microsoft/biogpt")
+# # model = T5ForConditionalGeneration.from_pretrained('laituan245/molt5-small')
+# # model = AutoModelForCausalLM.from_pretrained("ncfrey/ChemGPT-4.7M")
+data_collator = DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm=False)
+
+
+# torch.cuda.set_device(7)
+model.to(device)
+
+training_args = TrainingArguments(
+    output_dir='./g2_results_gen_single_gpt2_all', 
+    overwrite_output_dir = True ,      # output directory
+    num_train_epochs=5,              # total number of training epochs
+    per_device_train_batch_size=1,  # batch size per device during training
+    per_device_eval_batch_size=1,   # batch size for evaluation
+    warmup_steps=500,                # number of warmup steps for learning rate scheduler
+    weight_decay=0.01,               # strength of weight decay
+    logging_dir='./g2_logs_gen_single_gpt2_all',            # directory for storing logs
+    logging_steps=10,
+    save_total_limit=5
+    
+)
+
+
+trainer = Trainer(
+    model=model,                         # the instantiated model to be trained
+    args=training_args,                  # training arguments, defined above
+    train_dataset=train_dataset,         # training dataset
+    eval_dataset=val_dataset,             # evaluation dataset
+    data_collator=data_collator
+)
+
+trainer.train()
+fold = 1
+torch.save(model, str(fold)+"g2_gpt2_all.pt")
+from torch.utils.data import DataLoader
+from transformers import AdamW
+
+"""train_loader = DataLoader(train_dataset, batch_size=16, shuffle=True)
+
+
+optim = AdamW(model.parameters(), lr=5e-5)
+
+for epoch in range(3):
+    for batch in train_loader:
+        optim.zero_grad()
+        input_ids = batch['input_ids'].to(device)
+        attention_mask = batch['attention_mask'].to(device)
+        labels = batch['labels'].to(device)
+        outputs = model(input_ids, attention_mask=attention_mask, labels=labels)
+        loss = outputs[0]
+        loss.backward()
+        optim.step()
+
+model.eval()"""
+
+
+
+model=torch.load("/home/datalab/BioLLM/1g2_gpt2_all.pt")
+from transformers import pipeline
+from tqdm.auto import tqdm
+# device=torch.device("cuda")
+# # model.to(device)
+
+# # from transformers.pipelines.pt_utils import KeyDataset
+# #model.cuda()
+# # import csv 
+# model_name = "/home/datalab/BioLLM/results_gen_single_gpt2_all/checkpoint-189000"
+# model = AutoModelForCausalLM.from_pretrained(model_name)
+
+generator = pipeline(task="text-generation",  model=model.to('cpu'), tokenizer=tokenizer)
+# tokenizer_kwargs = {'padding':True,'truncation':True,'max_length':2048}
+# num_generations = 26
+# prompt = "label: '1'"
+# label_token = tokenizer.encode('1')[0]
+# generated = []
+# context = tokenizer.encode(prompt)
+# past = None
+# while True:
+#     input_ids = torch.tensor(context).unsqueeze(0).to(device)
+#     logits, past = model(input_ids=input_ids, past=past)
+#     next_token_logits = logits[:, -1, :]
+#     next_token_id = next_token_logits.argmax().item()
+#     if next_token_id == tokenizer.eos_token_id:
+#         break
+#     if next_token_id == label_token:
+#         generated.append(tokenizer.decode(next_token_id))
+#         context = [next_token_id]
+#     else:
+#         continue
+
+# generated_smiles = ''.join(generated)
+generated_text = [generator(text_inputs="1",prefix="<|startoftext|>", max_length=50)[0]['generated_text'] for i in range(5000)]
+with open('g2_results_smiles_gpt2_50000.txt', 'w') as file:
+    for text_and_class in generated_text:
+        file.write(text_and_class + "\n")
+k = 5  # set the value of k
+# with torch.no_grad():
+#     input_ids = tokenizer("O=C(NO)c1cccc(OCc2ccc(-c3ccccc3)cc2)", return_tensors='pt').input_ids.repeat(num_generations, 1)
+#     logits = model(input_ids.to(model.device)).logits
+#     top_k_logits, top_k_indices = torch.topk(logits, k, dim=-1)
+
+# # Print the top-k generated text for each prompt
+# for i, prompt in enumerate(generated_text):
+#     print(f"Prompt {i}: {prompt}")
+#     for j in range(k):
+#         generated_sequence = tokenizer.decode(top_k_indices[i][j], skip_special_tokens=True)
+#         print(f"Top-{j+1} sequence: {generated_sequence}")
+#     print("")
+# with open('generated_text.csv', 'w', newline='') as file:
+#     writer = csv.writer(file)
+#     writer.writerow(["Generated Text"])
+#     for text in generated_text:
+#         writer.writerow([text])
+
+#evaluating the classifier 
+
+
+
+# generator = pipeline(task="text-generation",  model=model, tokenizer=tokenizer)
+# tokenizer_kwargs = {'padding':True,'truncation':True,'max_length':512}
+# generator("0 $ ")
+# from transformers import pipeline
+# from tqdm.auto import tqdm
+# model=torch.load("/home/f20190969/BioLLM/fold0gpt2.pt")
+# test_samples, test_labels = readsmiles('/home/f20190969/BioLLM/generated_text_and_classes_1.txt')
+# # for i in range(50):
+# #     print(test_samples[i])
+# classifier = pipeline(task="text-classification",  model=model, tokenizer=tokenizer)
+# tokenizer_kwargs = {'padding':True,'truncation':True,'max_length':512}
+# predictions=[]
+# for i in range(50):
+#     predictions.append(classifier(test_samples[i],**tokenizer_kwargs))
+# print(predictions)
+
+
