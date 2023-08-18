9c8f7fe18e19b11fb472032810a962864f750507
Shreyas Bhat <56354373+Shreyas-Bhat@users.noreply.github.com>
2023-08-18 20:39:11 +0530

    Update nofeedback.py

--- nofeedback.py
+++ nofeedback.py
@@ -7,15 +7,12 @@ import random
 import json
 import sys
 import os
-from rdkit.Chem import Descriptors, QED
 from rdkit.Chem import rdMolDescriptors as rdmd
-
-#{"label": "1", "smiles": "O=c1[nH]c2cnc(-n3cnc4ccc(F)cc43)nc2n1Cc1cccc(F)c1F"}
 # Set up your API key and model parameters
 #model_engine = 'gpt-3.5-turbo' # You can choose a different model if desired
-model_engine = 'text-davinci-002'
+model_engine = 'text-davinci-003'
 temperature = 0.3 
-num_generations = 10# The number of times to generate new molecules and feed them back into the modelcond
+num_generations = 10
 import openai
 import jsonlines
 import time
@@ -30,98 +27,14 @@ sys.path.append(os.path.join(RDConfig.RDContribDir, 'SA_Score'))
 import sascorer
 
 
-directory_path = '/home/datalab/BioLLM/'
-target_file_path = '/home/datalab/BioLLM/drd2.jsonl'
-output_file_path = '/home/datalab/BioLLM/drd2n10nofeedback_data.csv'
+directory_path = ''
+target_file_path = ''
+output_file_path = ''
 target_molecules = []
 target_labels = []
 
-threshold_increment_frequency = 2
 data = []
-def calculate_docking_score(smiles):
-    try:
-        # Generate an RDKit molecule from the SMILES string
-        molecule = Chem.MolFromSmiles(smiles)
-        molecule = Chem.AddHs(molecule)
-    except Exception as e:
-        print(f"SMILES Parse Error: {e}. Skipping molecule: {smiles}")
-        return None
-    if molecule is not None:
-        # Generate 2D coordinates for the molecule
-        try:
-            AllChem.Compute2DCoords(molecule)
-        except Exception as e:
-            print(f"Compute2DCoords Error: {e}. Skipping molecule: {smiles}")
-            return None
-
-        # Generate a 3D conformation of the molecule using the ETKDG method
-        AllChem.EmbedMolecule(molecule, AllChem.ETKDG())
-
-        # Optimize the 3D conformation
-        try:
-            AllChem.MMFFOptimizeMolecule(molecule)
-        except Exception as e:
-            print(f"MMFFOptimizeMolecule Error: {e}. Skipping molecule: {smiles}")
-            return None
-
-        # Generate a PDB file from the molecule
-        pdb_filename = '/home/datalab/BioLLM/ligand.pdb'
-        writer = Chem.PDBWriter(pdb_filename)
-        writer.write(molecule)
-        writer.close()
-
-        # Run the gnina docking script
-        #/home/datalab/gnina --config JAK2_config.txt --ligand /home/datalab/BioLLM/chatgpt/${b}.sdf --out /home/datalab/BioLLM/chatgpt_output_sdf/${b}_out.sdf --log /home/datalab/BioLLM/chatgpt_active_output_logs/${b}_log.txt --cpu 4 --num_modes 1
-
-        #!./gnina -r /content/4IVA.pdb -l /content/ligand.smi --autobox_ligand /content/ligand.smi -o docked.sdf --seed 0
-        cmd = ['/home/datalab/gnina', '--config', 'JAK2_config.txt', '--ligand', '/home/datalab/BioLLM/ligand.pdb', '--out', 'output.sdf', '--log', '/home/datalab/BioLLM/threshold_output_log.txt', '--cpu', '4', '--num_modes', '1']
-        # 
-        #cmd = ['/home/datalab/gnina', '-r', '/home/datalab/BioLLM/4IVA.pdb', '-l', '/home/datalab/BioLLM/ligand.pdb', '--autobox_ligand', '/home/datalab/BioLLM/ligand.pdb', '-o', '/content/docked.txt', '--seed', '0']
-
-        print("Docking Command:", ' '.join(cmd))
-        # try:
-        #     subprocess.run(cmd, check=True)
-        # except subprocess.CalledProcessError as e:
-        #     print("Docking Error:", e)
-        #     return None
-        try:
-            subprocess.run(cmd, check=True, stderr=subprocess.PIPE)
-        except subprocess.CalledProcessError as e:
-            print("Docking process failed:", e)
-            print("Error output:", e.stderr)
-            return None
-
-
-        # subprocess.run(cmd, check=True)
-        
-        # TODO: Extract and return the docking score from the output files
-        import os
-
-        # Iterate over the files in the directory
-        for filename in os.listdir(directory_path):
-            if filename.endswith('.txt'):  # Consider only the text files
-                file_path = os.path.join(directory_path, filename)
-                with open(file_path, 'r') as file:
-                    lines = file.readlines()
-
-                    for i, line in enumerate(lines):
-                        if 'affinity' in line.lower() and 'cnn' in line.lower():
-                            third_next_line_values = lines[i + 3].split()
-                            if len(third_next_line_values) >= 4:
-                                try:
-                                    cnn_affinity = float(third_next_line_values[3].strip())
-                                    return cnn_affinity
-                                except ValueError:
-                                    pass
-        
-        return None
-
 for i in range(1, num_generations):
-    
-    
-    # if i % threshold_increment_frequency == 0 and i > 0:
-    #     threshold += threshold_increment
-    
     
     new_molecules = []
     num_molecules = 20
